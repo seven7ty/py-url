115345937568421c271f8892ddc7fbb27fec7814
itsmewulf <pa.przybyszewski@gmail.com>
2020-12-22 19:33:18 +0100

    create tasks.py

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import psycopg2.extras
 import flask
 import string
 import random
-import typing as t
+from collections import namedtuple
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app: flask.Flask = flask.Flask(__name__)
+Response = namedtuple('Response', ['json', 'code'])
 database = psycopg2.connect(dbname=os.getenv("DB_NAME"),
                             user=os.getenv("DB_USER"),
                             host=os.getenv("DB_HOST"),
@@ -21,19 +22,23 @@ def generate_slug(length: int = 5) -> str:
     return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(length))
 
 
-def create(body: dict) -> dict:
+def create(body: dict) -> Response:
     with database.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
         if 'link' not in body:
-            return {"success": False,
-                    "message": "Bad Request - please specify a link redirect in the request body"}
+            return Response({"success": False,
+                    "message": "Bad Request - please specify a link redirect in the request body"}, 400)
         if len(body['link']) > 100:
-            return {"success": False,
-                    "message": "Bad Request - link cannot be longer than 100 characters."}
-        slug = body['slug'] if 'slug' in body and len(body['slug']) < 50 else generate_slug()
+            return Response({"success": False,
+                    "message": "Bad Request - link cannot be longer than 100 characters."}, 400)
+        slug = body['slug'] if 'slug' in body and len(body['slug']) < 50 else generate_slug(10)
         cur.execute('SELECT link FROM links WHERE slug = %s', (slug,))
         res = cur.fetchone()
         if res:
-            return {"success": False, "message": "Bad Request - this slug already exists"}
+            return Response({"success": False, "message": "Bad Request - this slug already exists"}, 400)
+        cur.execute('INSERT INTO links VALUES (%%s %%s)', (body['link'], slug))
+        return Response({"success": True, "payload": {"link": body['link'], "slug": slug}}, 201)
+
+def get(slug: str) -> tuple()
 
 
 @app.route('/<slug>', methods=['GET', 'POST', 'DELETE'])
@@ -46,16 +51,8 @@ def redirect(slug):
                 return flask.jsonify({"success": True, "payload": {"link": res.link}}), 200
             return flask.jsonify({"success": False, "message": "Not Found - this slug does not exist"}), 404
         elif flask.request.method == 'POST':
-            body = flask.request.data
-            if 'link' not in body:
-                return flask.jsonify({"success": False,
-                                      "message": "Bad Request - please specify a link redirect in the request body"}), 400
-
-            cur.execute('SELECT link FROM links WHERE slug = %s', (slug,))
-            res = cur.fetchone()
-            if res:
-                return flask.jsonify({"success": False, "message": "Not Found - this slug already exists"})
-            cur.execute('INSERT INTO links VALUES (%%s %%s)', ())
+            response = create(flask.request.data)
+            return flask.jsonify(response.json), response.code
 
 
 @app.route('/', methods=['GET'])
