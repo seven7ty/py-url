eafc7c5122fe5b2659c47fae6ac1acdc8c13df38
itsmewulf <pa.przybyszewski@gmail.com>
2020-12-22 18:47:39 +0100

    basic skeleton

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 import os
-from flask import Flask, redirect
+import psycopg2
+import psycopg2.extras
+import flask
+from dotenv import load_dotenv
 
-app: Flask = Flask(__name__)
+load_dotenv()
+
+app: flask.Flask = flask.Flask(__name__)
+database = psycopg2.connect(dbname=os.getenv("DB_NAME"),
+                            user=os.getenv("DB_USER"),
+                            host=os.getenv("DB_HOST"),
+                            port=os.getenv("DB_PORT"),
+                            password=os.getenv("DB_PASSWORD"))
 
 
-@app.route('/<slug>')
+@app.route('/<slug>', methods=['GET'])
 def redirect(slug):
-    return redirect()
+    cur = database.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+    cur.execute('SELECT link FROM links WHERE slug = %s', (slug,))
+    res = cur.fetchone()
+    if hasattr(res, 'link'):
+        return flask.jsonify({"success": True, "payload": {"link": res.link}})
+    return flask.jsonify({"success": False, "message": "Not Found - this slug does not exist"})
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return flask.jsonify({})
 
 
 if __name__ == '__main__':
---
+++ setup.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS links (
+    slug VARCHAR(55),
+    link VARCHAR(105)
+)
