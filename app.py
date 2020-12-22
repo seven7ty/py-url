6ca815c09f50333c59d8a9f1944e8396e13abe3a
itsmewulf <pa.przybyszewski@gmail.com>
2020-12-22 18:52:48 +0100

    start on multiple methods

--- app.py
+++ app.py
@@ -14,14 +14,25 @@ database = psycopg2.connect(dbname=os.getenv("DB_NAME"),
                             password=os.getenv("DB_PASSWORD"))
 
 
-@app.route('/<slug>', methods=['GET'])
+@app.route('/<slug>', methods=['GET', 'POST', 'DELETE'])
 def redirect(slug):
-    cur = database.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
-    cur.execute('SELECT link FROM links WHERE slug = %s', (slug,))
-    res = cur.fetchone()
-    if hasattr(res, 'link'):
-        return flask.jsonify({"success": True, "payload": {"link": res.link}})
-    return flask.jsonify({"success": False, "message": "Not Found - this slug does not exist"})
+    with database.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
+        if flask.request.method == 'GET':
+            cur.execute('SELECT link FROM links WHERE slug = %s', (slug,))
+            res = cur.fetchone()
+            if hasattr(res, 'link'):
+                return flask.jsonify({"success": True, "payload": {"link": res.link}})
+            return flask.jsonify({"success": False, "message": "Not Found - this slug does not exist"})
+        elif flask.request.method == 'POST':
+            body = flask.request.data
+            if 'link' not in body:
+                return flask.jsonify({"success": False, "message": "Bad Request"}), 400
+            cur.execute('SELECT link FROM links WHERE slug = %s', (slug,))
+            res = cur.fetchone()
+            if res:
+                return flask.jsonify({"success": False, "message": "Not Found - this slug already exists"})
+            cur.execute('INSERT INTO links VALUES (%%s %%s)', ())
+
 
 
 @app.route('/', methods=['GET'])
